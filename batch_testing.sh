77c47d5eb9001203ca988fee8ceb938108832127
MQuaresma <miguelquaresma97@gmail.com>
2019-02-28 20:06:52 +0000

    Batch testing script

---
+++ batch_testing.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./main_py datasets/a_example.txt > a.txt
+./main_py datasets/b_lovely_landscapes.txt > b.txt
+./main_py datasets/c_memorable_moments.txt > c.txt
+./main_py datasets/d_pet_pictures.txt > d.txt
+./main_py datasets/e_shiny_selfies.txt > e.txt
